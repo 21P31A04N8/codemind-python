23d96d26cf7a801c0fc53b18ceb4fbbe108b1f49
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2022-10-31 15:50:32 +0530

    def primecount(n):     pc=0     for j in range(1,n+1):         if(n%j==0):             pc+=1     if(pc==2):         return n         return True     else:         return False m=int(input()) n=int(input()) for i in range(m,n+1):     if(primecount(i)):         print(primecount(i))

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,18 @@
+def primecount(n):
+    pc=0
+    for j in range(1,n+1):
+        if(n%j==0):
+            pc+=1
+    if(pc==2):
+        return n
+        return True
+    else:
+        return False
+m=int(input())
+n=int(input())
+for i in range(m,n+1):
+    if(primecount(i)):
+        print(primecount(i))
+        
+        
+            
