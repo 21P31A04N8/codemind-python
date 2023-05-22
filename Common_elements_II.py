237e5d05e09340b57d0c01e0c563ac34bd36d82f
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2023-05-22 22:32:26 +0530

    n,m=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l=[] l3=[] for i in l1:     if i not in l2:         l.append(i) for i in l2:     if i not in l1:         l.append(i) for i in l:     if i not in l3:         l3.append(i) print(*l3)

---
+++ Common_elements_II.py
@@ -0,0 +1,15 @@
+n,m=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l=[]
+l3=[]
+for i in l1:
+    if i not in l2:
+        l.append(i)
+for i in l2:
+    if i not in l1:
+        l.append(i)
+for i in l:
+    if i not in l3:
+        l3.append(i)
+print(*l3)
