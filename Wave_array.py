ad2477856fb193ecef904eb868b2e1c6defe58f8
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2023-05-26 10:02:44 +0530

    def wave(x,l):     c=0     if l[0]<l[1]:         for i in range(1,x-1,2):             if (l[i-1]<l[i]):                 c+=1             else:                 c=0                 return 0                 break         if c!=0:             return 1     else:         for i in range(1,x-1,2):             if (l[i-1]>l[i]):                 c+=1             else:                 c=0                 return 0                 break         if c!=0:             return 1 x = int(input()) l = list(map(int,input().split())) if wave(x,l):     print("yes") else:     print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -2,7 +2,7 @@ def wave(x,l):
     c=0
     if l[0]<l[1]:
         for i in range(1,x-1,2):
-            if (l[i-1]<l[i] and l[i]>l[i-1]):
+            if (l[i-1]<l[i]):
                 c+=1
             else:
                 c=0
@@ -12,7 +12,7 @@ def wave(x,l):
             return 1
     else:
         for i in range(1,x-1,2):
-            if (l[i-1]>l[i] and l[i]<l[i-1]):
+            if (l[i-1]>l[i]):
                 c+=1
             else:
                 c=0
