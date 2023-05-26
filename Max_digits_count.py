884377a8bc9c53e2012169305da0054fbf5efc4b
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2023-05-26 13:55:42 +0530

    def digit(n):     n=abs(n)     k=str(n)     return len(k) n=int(input()) lst=list(map(int,input().split())) l=[] cnt=0 for i in range(n):     m=digit(lst[i])     l.append(m) b=max(l) #print(l) for i in range(n):     if digit(lst[i])==b:         cnt+=1 print(cnt)

---
+++ Max_digits_count.py
@@ -0,0 +1,17 @@
+def digit(n):
+    n=abs(n)
+    k=str(n)
+    return len(k)
+n=int(input())
+lst=list(map(int,input().split()))
+l=[]
+cnt=0
+for i in range(n):
+    m=digit(lst[i])
+    l.append(m)
+b=max(l)
+#print(l)
+for i in range(n):
+    if digit(lst[i])==b:
+        cnt+=1
+print(cnt)
