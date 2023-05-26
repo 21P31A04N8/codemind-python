641fd89264bc35422e735214bf5d3bec1dcf1d0a
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2023-05-26 15:13:11 +0530

    n=int(input()) s=str(n) lst=list(s) k=set(s) if len(lst)==len(k):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,18 +1,8 @@
-def fun(d,num):
-    while num:
-        k=num%10
-        num=num//10
-        if d==k:
-            return True
-    return False
 n=int(input())
-s=0
-while n:
-    d=n%10
-    n=n//10
-    if fun(d,s):
-        print("Not Unique Number")
-        break
-    s=s*10+d
-else:
+s=str(n)
+lst=list(s)
+k=set(s)
+if len(lst)==len(k):
     print("Unique Number")
+else:
+    print("Not Unique Number")
