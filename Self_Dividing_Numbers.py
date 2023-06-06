aca9b093f756823abcea50e8040fe4268cc97d97
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2023-06-06 12:13:06 +0530

    def selfdiv(n):     c,dc=0,0     x=n     while(n):         r=n%10         c+=1         if(r==0):             break         if(x%r==0):             dc+=1         n=n//10     if(c==dc):         return 1     else:         return 0 a=int(input()) b=int(input()) for i in range(a,b+1):     if(selfdiv(i)):         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,14 +1,21 @@
-def selfdivnum(num):
-    temp=num
-    while temp:
-        d=temp%10
-        temp=temp//10
-        if d==0 or num%d!=0 :
-            return False
-    return True
-    
-n=int(input())
-m=int(input())
-for i in range(n,m+1):
-    if selfdivnum(i):
+def selfdiv(n):
+    c,dc=0,0
+    x=n
+    while(n):
+        r=n%10
+        c+=1
+        if(r==0):
+            break
+        if(x%r==0):
+            dc+=1
+        n=n//10
+    if(c==dc):
+        return 1
+    else:
+        return 0
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if(selfdiv(i)):
         print(i,end=" ")
+        
